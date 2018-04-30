fd00e333347d0a7113e470922f31bf2c03267be2
Harpa Hreinsdottir <harpah16@ru.is>
2018-04-30 14:28:50 +0000

    delete

--- BookCave/Models/EntityModels/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace BookCave.Models.EntityModels
-{
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    
-    }
-}
--- BookCave/Models/ViewModels/BookListViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookCave.Models.ViewModels
-{
-    public class BookListViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    
-    }
-}
