c0ee0e5c4a722403b11f02fcd32b2a6611de9b05
Harpa Hreinsdottir <harpah16@ru.is>
2018-04-30 11:24:50 +0000

    new mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KungFu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
